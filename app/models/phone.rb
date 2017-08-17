4eeb1bf3d68424c403c59620107cf67db25bcfda
Rafael Fernando dos Santos <rfs.rafael23@gmail.com>
2017-08-17 14:29:31 -0300

    Adicionada pesquisa pelo PgSearch

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,10 @@
 class Phone < ApplicationRecord
+  include PgSearch
   belongs_to :person
   belongs_to :division
+
+  pg_search_scope :search, :against => [:ddr, :ramal],
+                  :associated_against => {division: [:name, :ancestors],
+                                          person: [:name]},
+                  :using => {tsearch: {:dictionary => "portuguese"}}
 end
