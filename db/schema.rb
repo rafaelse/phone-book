375156d52474a9e613cefc94f34838d456f9c008
Rafael Fernando dos Santos <rfs.rafael23@gmail.com>
2017-09-12 16:41:58 -0300

    Comecando a implementar usuario/login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817194912) do
+ActiveRecord::Schema.define(version: 20170912135247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20170817194912) do
     t.datetime "updated_at",  null: false
     t.index ["division_id"], name: "index_phones_on_division_id", using: :btree
     t.index ["person_id"], name: "index_phones_on_person_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.boolean  "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "phones", "divisions"
