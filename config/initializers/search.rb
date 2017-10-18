831bd3b31c2eef93d52a99d0341eb2d6b6d4bd88
Rafael Fernando dos Santos <rfs.rafael23@gmail.com>
2017-10-18 12:53:48 -0200

    Funcao scroll top

--- config/initializers/search.rb
+++ config/initializers/search.rb
@@ -1,5 +1,5 @@
 PgSearch.multisearch_options = {
-    :using => {:tsearch => {dictionary: 'portuguese', prefix: true},
+    :using => {:tsearch => {dictionary: 'portuguese', prefix: true, any_word: true},
                :dmetaphone => {},
                :trigram => {threshold: 0.5}},
     :ignoring => :accents
