7b9381b602700beeb9775978b46588b0b40a3364
Rafael Fernando dos Santos <rfs.rafael23@gmail.com>
2017-10-24 16:09:15 -0200

    Ultima busca fica na barra e pesos regulares para as fontes das listagens

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,8 +14,8 @@ class SearchController < ApplicationController
                                   index_name: [Phone, Person, Division],
                                   indices_boost: {Phone => 1, Division => 2, Person => 3},
                                   operator: "or",
-                                  misspellings: {below: 3, edit_distance: 5},
-                                  body_options: {min_score: 5},
+                                  misspellings: {below: 3, edit_distance: 3},
+                                  body_options: {min_score: 2},
                                   fields: [{name: :word_start}, :ddr, :branch]
 
       @active_class = results.results.empty? ? nil : results.results.first.class
