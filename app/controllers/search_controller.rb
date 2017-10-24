b8a7a66683cd3a080811e12f67831b788183934a
Rafael Fernando dos Santos <rfs.rafael23@gmail.com>
2017-10-24 13:24:12 -0200

    Correcao busca para erros de ortografia

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,18 +11,18 @@ class SearchController < ApplicationController
     unless params[:term].blank?
       # @phones = Phone.search(params[:term])
       results = Searchkick.search params[:term],
-                                   index_name: [Phone, Person, Division],
-                                   indices_boost: {Phone => 1, Division => 2, Person => 3},
-                                   operator: "or",
-                                   misspellings: {below: 5, edit_distance: 5},
-                                   body_options: {min_score: 5},
-                                   fields: [{name: :word_start}, :ddr, {branch: :exact}]
+                                  index_name: [Phone, Person, Division],
+                                  indices_boost: {Phone => 1, Division => 2, Person => 3},
+                                  operator: "or",
+                                  misspellings: {below: 3, edit_distance: 5},
+                                  body_options: {min_score: 2},
+                                  fields: [{name: :word_start}, :ddr, :branch]
 
-      unless results.response["hits"]["total"] == 0
-        first_item_type = results.response["hits"]["hits"].first["_type"]
-        item_types = results.response["hits"]["hits"].collect {|item| item["_type"]}
-        item_types_count = item_types.uniq.map {|type| [type, item_types.count(type)]}.to_h
-        @results = {items: results.results, first_item_type: first_item_type, item_types_count: item_types_count}
+      @active_class = results.results.empty? ? nil : results.results.first.class
+      results.results.each do |result|
+        @phones << result if result.class == Phone
+        @people << result if result.class == Person
+        @divisions << result if result.class == Division
       end
     end
   end
